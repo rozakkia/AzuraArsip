cf69898b8b315219a2227b20e14f0cf225178f36
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-23 15:37:14 +0700

    add migrations table

---
+++ migrations/201909231514-CreateUserTable.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('tbusers', {
+            id_user: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+            },
+            username: {
+                allowNull: true,
+                type: Sequelize.STRING
+            },
+            name: {
+                allowNull: true,
+                type: Sequelize.STRING
+            },
+            password: {
+                allowNull: true,
+                type: Sequelize.STRING
+            },
+            level: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+        })
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Users');
+    }
+};
