7fbbaf9a01548cef891e8df2533ed0631ca5c5a3
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-24 17:03:47 +0700

    adding users and login route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 let index = require('../controllers/index');
+let user = require('../controllers/user');
 
 /* GET home page. */
 router.get('/', index.get_index);
+router.get('/login', user.get_login);
+router.get('/users', user.get_users);
 
 module.exports = router;
