a4e3678dcd406419baad88209c73cd5134895e6c
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-24 10:54:18 +0700

    Adding Template views and config it

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ var router = express.Router();
 let index = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', index.get_index);
 
 module.exports = router;
