2e36c5c338f0fe0795437cff9a5e6b9c8e8b1be9
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-24 20:11:55 +0700

    updates controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,18 @@
+
+const models = require('../models')
+
 exports.get_login = function(req, res, next) {
     res.render('auth/login', { title: 'Halaman Masuk' });
   }
 
 exports.get_users = function(req, res, next) {
 res.render('user/index', { title: 'Data User' });
+}
+
+exports.adding_user = function(req,res,next) {
+  return models.User.create({
+      email: req.body.lead_email
+  }).then(lead=> {
+      res.redirect('/leads');  
+  })
 }
