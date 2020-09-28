7720a734362359d4a41055060ca869e39a410eee
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-28 11:45:15 +0700

    update logout route

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ let user = require('../controllers/user');
 router.get('/', index.get_index);
 router.get('/login', user.get_login);
 router.get('/users', user.get_users);
+router.post('/users', user.adding_user);
+
+router.get('/logout', user.logout);
 
 module.exports = router;
