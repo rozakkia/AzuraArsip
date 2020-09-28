55d1707f2dde37156fd1f4ec61dd429e4c7648f9
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-28 17:05:14 +0700

    add middleware session

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ var router = express.Router();
 let index = require('../controllers/index');
 let user = require('../controllers/user');
 
-/* GET home page. */
-router.get('/', index.get_index);
-router.get('/login', user.get_login);
-router.get('/users', user.get_users);
-router.post('/users', user.adding_user);
+let {isLoggedIn} = require('../middleware/hasAuth.js')
+let {hasAuthAdmin} = require('../middleware/hasAuth.js')
 
-router.get('/logout', user.logout);
+/* GET home page. */
+router.get('/', isLoggedIn, index.get_index);
+router.get('/login', user.get_login);
+router.post('/login', user.login);
+
+router.get('/users', isLoggedIn, user.get_users);
+router.post('/users', isLoggedIn, user.adding_user);
+
+router.get('/logout', isLoggedIn, user.logout);
 
 module.exports = router;
