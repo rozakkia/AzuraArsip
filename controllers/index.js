6d98cd81b9cd1b795b74673addc8f95975b87f5e
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-28 17:12:10 +0700

    Update controller with middleware and validators

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
-exports.get_index = function(req, res, next) {
-    res.render('index', { title: 'Dashboard' });
+
+
+exports.get_index = function( req, res, next) {
+    res.render('index', { title: 'Dashboard', user: req.user });
   }
--- controllers/user.js
+++ controllers/user.js
@@ -4,35 +4,51 @@ let bcrypt = require("bcrypt");
 const passport = require('passport');
 const myPassport = require('../passport_setup')(passport);
 let flash = require('connect-flash');
+const {isEmpty} = require('lodash');
+const { validateUser } = require('../validators/user');
 
+// Views Controller
 exports.get_login = function(req, res, next) {
-    res.render('auth/login', { title: 'Halaman Masuk' });
-  }
+  res.render('auth/login', { title: 'Halaman Masuk' , user: req.user });
+}
 
 exports.get_users = function(req, res, next) {
-res.render('user/index', { title: 'Data User' });
+  res.render('user/index', { title: 'Data User', user: req.user , formData: {}, errors: {} });
 }
+
+exports.rerender_get_users = function(errors, req, res, next) {
+  res.render('user/index', { title: 'Data User', user: req.user , formData: req.body, errors: errors });
+}
+
 
 const generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 exports.adding_user = function(req, res, next) {
-  let newUser;
-  console.log("Email: ", generateHash(req.body.password));
-  newUser = models.User.build({
-    username: req.body.username,
-    name: req.body.name, 
-    password: generateHash(req.body.password),
-    level: req.body.level
-  });
-  return newUser.save().then(result => {
-    passport.authenticate('local', {
-      successRedirect: "/users",
-      failureRedirect: "/",
-      failureFlash: true
-    })(req, res, next);
+  let errors = {};
+  return validateUser(errors, req).then(error => {
+    if (!isEmpty(errors)) {
+      rerender_get_users(errors, req, res, next);
+    } else {
+      return models.User.create({
+          username: req.body.username,
+          name: req.body.name, 
+          password: generateHash(req.body.password),
+          level: req.body.level
+      }).then(user => {
+          res.redirect('/users');  
+      })
+    }
   })
+}
+
+exports.login = function(req,res,next){
+  passport.authenticate('local', {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true
+  })(req, res, next);  
 }
 
 exports.logout = function(req, res, next) {
