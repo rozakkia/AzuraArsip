a33c414df8b51a6f047827faae242d6f4b645419
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-10-02 16:56:22 +0700

    update routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 let index = require('../controllers/index');
 let user = require('../controllers/user');
-let settings = require('../controllers/settings');
 
 let {isLoggedIn} = require('../middleware/hasAuth.js')
 let {hasAuthAdmin} = require('../middleware/hasAuth.js')
@@ -13,14 +12,13 @@ router.get('/', isLoggedIn, index.get_index);
 router.get('/login', user.get_login);
 router.post('/login', user.login);
 
+// USERS //
 router.get('/users', isLoggedIn, user.get_users);
 router.post('/users', isLoggedIn, user.create_user);
 router.get('/users/:user_id', isLoggedIn, user.get_detail);
 router.post('/users/:user_id/edit', isLoggedIn, user.edit_user);
 router.post('/users/:user_id/delete', isLoggedIn, user.delete_user);
 
-
-router.get('/settings', isLoggedIn, settings.get_settings);
 
 router.get('/logout', isLoggedIn, user.logout);
 
