f9c915136c3865d03853a7f3fb6e386ccc74fcc3
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-10-26 16:46:56 +0700

    update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "azuraarsip",
+    "database": "azura",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
