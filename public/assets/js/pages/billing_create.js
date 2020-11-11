242fc46f4bf1a9863800cfd80b2ed6bfd4a2c01b
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-11 14:25:36 +0700

    initial js billing page, handle POST and alert

---
+++ public/assets/js/pages/billing_create.js
@@ -0,0 +1,41 @@
+$("#formDetail").on("submit", function(e){
+    var formData = {
+        idBill : $('#idBill').val(),
+        keterangan : $("#keterangan").val(),
+        BankAccountId :  $("#bank").val()
+    }
+    e.preventDefault();
+    $.ajax({
+        type        : "POST",
+        contentType : "application/json",
+        url         : "update-created",
+        data        : JSON.stringify(formData),
+        dataType    : 'json',
+        success : function(alerts) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Data Tersimpan',
+                    icon: 'success',
+                    timer: 2500,
+                    showConfirmButton: false
+                });
+            },10);
+            window.setTimeout(function(){ 
+               location.reload();
+            } ,2500);   
+        },
+        error : function(e) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Gagal',
+                    icon: 'error',
+                    text: e,
+                    timer: 2500,
+                    showConfirmButton: false
+                }, function(){
+                    location.reload();
+                });
+            },10);
+        }
+    })
+})
