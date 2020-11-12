43fa533c4fa00528718d14dfb68b9d19b617551c
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-12 16:54:26 +0700

    add jquery for creating details bill

--- public/assets/js/pages/billing_create.js
+++ public/assets/js/pages/billing_create.js
@@ -39,3 +39,45 @@ $("#formDetail").on("submit", function(e){
         }
     })
 })
+
+$("#formCreateDetail").on("submit", function(e){
+    var formData = {
+        idBill : $('#idBill').val(),
+        keterangan : $("#keterangan").val(),
+        jumlah :  $("#jumlah").val()
+    }
+    e.preventDefault();
+    $.ajax({
+        type        : "POST",
+        contentType : "application/json",
+        url         : "create-detail",
+        data        : JSON.stringify(formData),
+        dataType    : 'json',
+        success : function(alerts) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Data Tersimpan',
+                    icon: 'success',
+                    timer: 2500,
+                    showConfirmButton: false
+                });
+            },10);
+            window.setTimeout(function(){ 
+               location.reload();
+            } ,2500);   
+        },
+        error : function(e) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Gagal',
+                    icon: 'error',
+                    text: e,
+                    timer: 2500,
+                    showConfirmButton: false
+                }, function(){
+                    location.reload();
+                });
+            },10);
+        }
+    })
+})
