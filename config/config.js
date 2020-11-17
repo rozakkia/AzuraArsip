a286567087dc82e13d08962603c377dc84bb5a9b
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-17 10:48:11 +0700

    Revert "add dotenv config"
    This reverts commit e32b25d6e3dff5a8bc788e7ec6d90cb9f4641ecb.

--- config/config.js
+++ config/config.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
-
 module.exports = {
   "development": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
+    "username": "root",
+    "password": null,
+    "database": "azura1",
+    "port":"3306",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
