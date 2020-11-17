e32b25d6e3dff5a8bc788e7ec6d90cb9f4641ecb
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-17 10:14:12 +0700

    add dotenv config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "azura1",
-    "port":"3306",
-    "host": "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
