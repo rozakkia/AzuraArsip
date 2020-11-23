07bedda9b44e6247876b7c200754a724e56398cc
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-23 15:47:01 +0700

    update

--- public/assets/js/pages/billing_create.js
+++ public/assets/js/pages/billing_create.js
@@ -1,8 +1,9 @@
 $("#formDetail").on("submit", function(e){
     var formData = {
-        idBill : $('#idBill').val(),
+        idNum : $('#idNum').val(),
         keterangan : $("#keterangan").val(),
-        BankAccountId :  $("#bank").val()
+        BankAccountId :  $("#bank").val(),
+        stat : "1"
     }
     e.preventDefault();
     $.ajax({
@@ -44,7 +45,8 @@ $("#formCreateDetail").on("submit", function(e){
     var formData = {
         idBill : $('#idBill').val(),
         keterangan : $("#keterangan").val(),
-        jumlah :  $("#jumlah").val()
+        jumlah :  $("#jumlah").val(),
+        harga :  $("#harga").val()
     }
     e.preventDefault();
     $.ajax({
@@ -67,6 +69,91 @@ $("#formCreateDetail").on("submit", function(e){
             } ,2500);   
         },
         error : function(e) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Gagal',
+                    icon: 'error',
+                    text: e,
+                    timer: 2500,
+                    showConfirmButton: false
+                }, function(){
+                    location.reload();
+                });
+            },10);
+        }
+    })
+})
+
+$("#formUpdate").on("submit", function(e){
+    var formData = {
+        idNum : $("#idNum").val(),
+        keterangan : $("#keteranganUp").val(),
+        BankAccountId : $("#rekeningUp").val(),
+        stat: "1"
+    }
+    e.preventDefault();
+    $.ajax({
+        type        : "POST",
+        contentType : "application/json",
+        url         : "update-created",
+        data        : JSON.stringify(formData),
+        dataType    : 'json',
+        success : function(Alerts) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Data Tersimpan',
+                    icon: 'success',
+                    timer: 2500,
+                    showConfirmButton: false
+                });
+            },10);
+            window.setTimeout(function(){ 
+               location.reload();
+            } ,2500);   
+        },
+        error : function(e) {
+            console.log(e)
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Gagal',
+                    icon: 'error',
+                    text: e,
+                    timer: 2500,
+                    showConfirmButton: false
+                }, function(){
+                    location.reload();
+                });
+            },10);
+        }
+    })
+})
+
+$("#formDelete").on("submit", function(e){
+    var formData = {
+        idNum : $("#idNum").val()
+    }
+    e.preventDefault();
+    $.ajax({
+        type        : "POST",
+        contentType : "application/json",
+        url         : "delete",
+        data        : JSON.stringify(formData),
+        dataType    : 'json',
+        success : function(Alerts) {
+            setTimeout(function () {  
+                swal.fire({
+                    title: 'Data Terhapus',
+                    icon: 'success',
+                    timer: 2500,
+                    showConfirmButton: false
+                });
+            },10);
+            window.setTimeout(function(){ 
+               location.replace("/settings/core");
+            } ,2500);   
+        },
+        error : function(e) {
+            console.log(e)
             setTimeout(function () {  
                 swal.fire({
                     title: 'Gagal',
