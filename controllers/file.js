be551fdf33e3c6908fadb1645ca7e3a1a3b0ed8e
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-11-24 12:58:23 +0700

    update

--- controllers/file.js
+++ controllers/file.js
@@ -1,6 +1,4 @@
 const path = require("path"); 
-const fs = require('fs');
-const PdfReader = require('pdfreader')
 
 exports.get_file = function(req, res, next) {
     var tempFile= path.join(process.cwd(), '/public/assets/file-surat/' + req.params.file_name)
